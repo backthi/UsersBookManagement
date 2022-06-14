e901e4b17d7524262c93729474357458f458c320
backthi <backthidevi@gmail.com>
2022-06-14 14:41:16 +0530

    Updated with Dockerization

--- routes/user.js
+++ routes/user.js
@@ -3,29 +3,6 @@ const routes = express.Router()
 const user_Schema = require('../model/user')
 const userCont = require('../controller/user')
 
-// routes.get('/', async(req,res) => {
-//     try
-//     {
-//         const user = await userSchema.find()
-//         res.json(user)
-//     }catch(err){
-//         res.send('Error ' + err)
-//     }
-// });
-
-// routes.post('/', async(req,res) => {
-//     const userData = new userSchema({
-//         name: req.body.name,
-//         role: req.body.role,
-//         date_joined: req.body.date_joined
-//     })
-//     try{
-//         const userData1 =  await userData.save() 
-//         res.json(userData1)
-//     }catch(err){
-//         res.send('Error ' + err)
-//     }
-// });
 
 // Create a new User
 routes.post('/', userCont.create);
